aed7eb84868a54cecb4c3be175e857d60f1aa85d
mayo <88555602+mayobami@users.noreply.github.com>
2026-03-13 23:35:04 +0000

    Update kth-largest-element-in-an-array.py

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
@@ -13,3 +13,15 @@ class Solution:
 
         # The root of the min-heap is the kth largest
         return min_heap[0]
+
+
+# Brute force - O(n log n)
+# "The simplest approach is to sort and return the kth element"
+nums.sort(reverse=True)
+return nums[k-1]
+
+#Or use heap library
+import heapq
+# "But we can do better with a min-heap of size k - O(n log k)"
+# This is faster when k is much smaller than n
+return heapq.nlargest(k, nums)[-1]
