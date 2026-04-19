eee5f82d3b5c4ad744f6d395d29bebe036f5ee65
mayo <88555602+mayobami@users.noreply.github.com>
2026-04-19 13:15:14 +0100

    Time: 119 ms (24.89%) | Memory: 20.5 MB (34.23%) - LeetSync

--- 463-island-perimeter/island-perimeter.py
+++ 463-island-perimeter/island-perimeter.py
@@ -1,26 +1,35 @@
+from collections import deque
 class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
         rows,cols = len(grid), len(grid[0])
-        self.perimeter=0
+        perimeter=0
         visit=set()
-        def dfs(r,c):
-            if r<0 or c<0 or r>=rows or c>=cols or grid[r][c] == 0:
-                self.perimeter += 1
-                return
-            if (r,c) in visit:
-                return
-
-            visit.add((r,c))
-            dfs(r+1,c)
-            dfs(r-1,c)
-            dfs(r,c+1)
-            dfs(r,c-1)
 
         for r in range(rows):
             for c in range(cols):
-                if grid[r][c]==1: 
-                    dfs(r,c)
-                    return self.perimeter
+                if grid[r][c]==1:   
+                     # Start BFS only when we find the island
+                    q=deque([(r,c)])
+                    visit.add((r,c)) # necessary to add the first r,c to visit set manually
+
+                    while q:
+                        r,c = q.popleft()
+                        directions = [[1,0],[-1,0],[0,1],[0,-1]]
+
+                        for dr,dc in directions:
+                            nr,nc = r+dr, c+dc
+
+                             # 1. If neighbor is out of bounds or water -> +1 Perimeter
+                            if nr < 0 or nr >= rows or nc < 0 or nc >= cols or grid[nr][nc] == 0:
+                                perimeter += 1
+
+                            elif (nr, nc) not in visit:
+                                visit.add((nr,nc))
+                                q.append((nr,nc))
+                    return perimeter                             
+
+                        
+
 
                 
     
